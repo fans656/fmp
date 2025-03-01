05977b7da0253928b9c10dcb3417d17aa7d12d23
fans656 <fans656@yahoo.com>
2025-03-01 21:24:10 +0800

    add command line arguments for width, height, x, y, etc

--- fmp/__main__.py
+++ fmp/__main__.py
@@ -8,17 +8,34 @@ from .ui import Player
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('-c', '--config')
-    parser.add_argument('files', nargs='+')
+    parser.add_argument('-c', '--config', help='Configuration file to use')
+    parser.add_argument('--width', type=int, default=800, help='Window width')
+    parser.add_argument('--height', type=int, default=600, help='Window height')
+    parser.add_argument('--x', type=int, default=None, help='Window x position')
+    parser.add_argument('--y', type=int, default=None, help='Window y position')
+    parser.add_argument('file', nargs='+', help='File(s) to play')
     args = parser.parse_args()
 
     app = QApplication(sys.argv)
 
+    desktop_rect = app.primaryScreen().availableGeometry()
+
+    width = args.width
+    height = args.height
+
+    x = args.x
+    if x is None:
+        x = desktop_rect.left() + (desktop_rect.width() - width) // 2
+    y = args.y
+    if y is None:
+        y = desktop_rect.top() + (desktop_rect.height() - height) // 2
+
     player = Player(
-        files=args.files,
+        files=args.file,
         config_path=args.config,
     )
-    player.resize(800, 600)
+    player.resize(width, height)
+    player.move(x, y)
     player.show()
 
     sys.exit(app.exec())
